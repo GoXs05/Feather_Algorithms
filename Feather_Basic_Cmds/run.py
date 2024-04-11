8496549b3df8443c5e616ce7914da572442f4a57
GoXs05 <gokulsriram992005@gmail.com>
2024-04-10 20:46:57 -0500

    Updated run.py to have text and voice control

--- Feather_Basic_Cmds/run.py
+++ Feather_Basic_Cmds/run.py
@@ -1,26 +1,49 @@
 from Vosk import speech_recog as sr
 from Llava import model as llava_model
 
-def run_program():
+import argparse
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='basic controls')
+    parser.add_argument('-use_speech', default=False, action='store_const', const=True, help='Use speech recognition or not?')
+
+    args = parser.parse_args()
+    return args
+
+def run_voice_program():
+    print("Please tell me what you want your drone to do!")
     print("Listening...")
-    while True: 
-        try:
-            speech = sr.listen()
-        except:
-            print("I'm sorry. I don't understand")
-            run_program()
-        else:
-            if speech != "" and speech != "exit":
-                print(speech)
-                commands = []
-                commands = llava_model.analyze_speech(speech)
-                for c in commands:
-                    print(c)
-                print("Listening...")
-            if speech == "exit":
-                print(speech)
-                print("Exiting the program")
-                exit()
+    speech = sr.listen()
+    if speech != "" and speech != "exit":
+        print(speech)
+        commands = []
+        commands = llava_model.analyze_speech(speech)
+        for c in commands:
+            print(c)
+    if speech == "exit":
+        print(speech)
+        print("Exiting the program!")
+        exit()
+
+def run_text_program():
+    speech = input('Please enter what you want your drone to do:\n')
+    if speech != "" and speech != "exit":
+        commands = []
+        commands = llava_model.analyze_speech(speech)
+        for c in commands:
+            print(c)
+    if speech == "exit":
+        print(speech)
+        print("Exiting the program!")
+        exit()
+
 
 if  __name__ == '__main__':
-    run_program()
+    args = parse_args()
+    print(args.use_speech)
+
+    while True:
+        if args.use_speech:
+            run_voice_program()
+        else:
+            run_text_program()
